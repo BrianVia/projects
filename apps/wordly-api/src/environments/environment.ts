9ac42fa77136acb1b806195b16c62ab7c3d0fb3c
Brian Via <brian.via.dev@gmail.com>
2022-09-13 13:47:32 -0400

    create wordly-api express app

---
+++ apps/wordly-api/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://uabaxagehyyyzdcgubln.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhYmF4YWdlaHl5eXpkY2d1YmxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIzNjk2MzMsImV4cCI6MTk2Nzk0NTYzM30.jPKmtDIa7LO_UGJaZpAtTc4InW0WF04lR1L2f6TZOoQ',
+};
