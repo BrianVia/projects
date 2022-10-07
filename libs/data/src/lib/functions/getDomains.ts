58ba2a4ecd0bb42ec6142738be4369e03cd7e93a
Brian Via <brian.via.dev@gmail.com>
2022-10-06 21:51:36 -0400

    Add logic to not upsert domains we already have, since they won't change.

--- libs/data/src/lib/functions/getDomains.ts
+++ libs/data/src/lib/functions/getDomains.ts
@@ -4,6 +4,7 @@ import { Logger } from '@wordly-domains/logger';
 import { ParkIOAPIResponse, ParkIODomain, Domain } from '../interfaces';
 import { supabase } from '../services';
 import { WORDS } from '../data';
+import { sleep } from '../utilities';
 
 const LOG_LEVEL = process.env.LOG_LEVEL || 'INFO';
 const logger = new Logger();
@@ -19,13 +20,58 @@ export async function getDomains() {
       return Promise.reject('could not fetch domains from Park.IO');
     });
 
-  domains.forEach(async (domain: ParkIODomain) => {
-    await loadDomain(domain, supabase);
-  });
+  const newDomains = await filterOutDomainsWeHave(domains);
 
-  return new Promise((resolve) => {
-    resolve('Domains fetched and loaded into DB');
+  if (newDomains.length > 0) {
+    logger.info(`Uploading ${newDomains.length} domains`);
+    newDomains.forEach(async (domain: ParkIODomain) => {
+      await loadDomain(domain, supabase);
+    });
+    return new Promise((resolve) => {
+      logger.info('Domains fetched and loaded into DB');
+      resolve('Domains fetched and loaded into DB');
+    });
+  } else {
+    return new Promise((resolve) => {
+      logger.info(`No new domains loaded.`);
+      resolve('No new domains loaded.');
+    });
+  }
+}
+
+async function filterOutDomainsWeHave(
+  domains: ParkIODomain[]
+): Promise<ParkIODomain[]> {
+  const domainNamesWeAlreadyHave = await getAvailableDomains().then((data) => {
+    return data.map((domain) => domain.name);
   });
+  const newDomains = domains.filter(
+    (domain) => !domainNamesWeAlreadyHave.includes(domain.name)
+  );
+  logger.info(
+    `Removed ${
+      domainNamesWeAlreadyHave.length - newDomains.length
+    } domains from insert payload.`
+  );
+  return newDomains;
+}
+
+async function getAvailableDomains() {
+  const availableDomains: Domain[] = [];
+  logger.debug('getAvailableDomains()');
+  logger.debug('querying domains table');
+  for (let i = 0; i < 4; i++) {
+    const { data, error, status } = await supabase
+      .from<Domain>('domains')
+      .select('name, tld, date_available, subwords')
+      .limit(1000)
+      .range(i * 1000, (i + 1) * 1000);
+    sleep(1000);
+    availableDomains.push(...(data || []));
+  }
+
+  logger.debug(`Retrieved ${availableDomains.length} domains from DB`);
+  return availableDomains;
 }
 
 async function getParkIODomains(page?: number, limit?: number) {
