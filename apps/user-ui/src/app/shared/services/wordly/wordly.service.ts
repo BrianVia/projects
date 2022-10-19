ef62e3c81a3155332ab2c1da11ed2b527bc444d7
Brian Via <brian.via.dev@gmail.com>
2022-10-19 14:13:27 -0400

    Fix a typo you idiot

--- apps/user-ui/src/app/shared/services/wordly/wordly.service.ts
+++ apps/user-ui/src/app/shared/services/wordly/wordly.service.ts
@@ -10,7 +10,7 @@ import { Profile } from '../../interaces/profile';
 export class WordlyService {
   private http: HttpClient;
   private BASE_URL = environment.production
-    ? 'https://api.wordly-domains/api/v1'
+    ? 'https://api.wordly.domains/api/v1'
     : 'http://localhost:8080/api/v1';
 
   constructor(http: HttpClient) {
