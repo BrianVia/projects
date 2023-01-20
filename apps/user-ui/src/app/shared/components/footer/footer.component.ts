5761c429bcb7171685270578b018044c7b3b9e47
Brian Via <brian.via.dev@gmail.com>
2023-01-20 12:20:18 -0500

    Rename things

--- apps/user-ui/src/app/shared/components/footer/footer.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'user-ui-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css'],
-})
-export class FooterComponent {}
