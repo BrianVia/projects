adbb7df63c85e8a56fcccf9d3662f8b88f777acb
Brian Via <brian.via.dev@gmail.com>
2023-05-15 12:40:39 -0400

    Add shell blog app

---
+++ apps/blog/astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import solid from '@astrojs/solid-js';
+import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  outDir: '../../dist/apps/blog',
+  integrations: [react(), solid(), svelte(), tailwind()],
+});
