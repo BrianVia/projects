dfa367b9eb89fb55b2d54da6218995fc763de37e
Brian Via <brian.via.dev@gmail.com>
2023-06-11 22:28:53 -0400

    FUCK IT RUN EM HOURLY

--- apps/wishlist-alerts-api/src/api/cronjobs/cronjobs.ts
+++ apps/wishlist-alerts-api/src/api/cronjobs/cronjobs.ts
@@ -9,7 +9,7 @@ export class cronjobs {
     console.log('Scheduling cron jobs...');
 
     console.log('Monitoring Wishlists Daily at 2:00 AM UTC');
-    cron.schedule('0 2 * * *', async () => {
+    cron.schedule('0 * * * *', async () => {
       // UTC Time
       const ran = await monitoringService.monitorWishlists('daily');
     });
