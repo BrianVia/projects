011d4144a19c3be0abfcea49862abdb716afb350
Brian Via <brian.via.dev@gmail.com>
2023-06-12 13:57:02 -0400

    Just say screw it an run at startup

--- apps/wishlist-alerts-api/src/main.ts
+++ apps/wishlist-alerts-api/src/main.ts
@@ -30,12 +30,11 @@ const cronHandler = new cronjobs();
 console.log('Cron jobs scheduled to run...');
 cronHandler.scheduleJobs();
 
-// monitoringService.monitorWishlists('daily');
-
-//
+monitoringService.monitorWishlists('daily'); // Just run at startup in the meantime
 
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
   logger.info(`Listening at http://localhost:${port}/api`);
 });
+
 server.on('error', logger.error);
